564905db7bd3e545b456ba66e3e865ead867ed2a
Shubhamdhiman16 <dhimanshubham1221@gmail.com>
2026-02-10 19:00:21 +0530

    remove some folder

--- backend/models/Freelancer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const freelancerSchema = new mongoose.Schema({
-    name: String,
-    skill: String,
-    price: Number
-});
-
-module.exports = mongoose.model("Freelancer", freelancerSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-// middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // ⭐ REQUIRED
 
 // routes
-const freelancerRoutes = require("./routes/freelancerRoutes");
-app.use("/api/freelancers", freelancerRoutes);
+app.use("/api/auth", require("./routes/authRoutes"));
 
-// db
-mongoose.connect("mongodb://127.0.0.1:27017/freelancerDB")
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000 🚀");
+  console.log("Server running on port 5000 🚀");
 });
